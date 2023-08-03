ca362da51da04fdadb1be3379d8334d050a54545
22037707 <22037707@DESKTOP-MVJO8QL>
2023-08-03 23:11:54 +0800

    Source

--- C206_CaseStudy/src/MoneyExchange.java
+++ C206_CaseStudy/src/MoneyExchange.java
@@ -20,40 +20,22 @@ public class MoneyExchange {
 		Users softwareEng = new Users("Asmond","Software Engineer",true,"002");
 		accList.add(new Account(softwareEng,"asmond0811","12345",9338707));
 		
-		
-	}
-
-	public static void loginMenu() {
 		System.out.println("Welcome to RemitNow !!!");
 		System.out.println("1. Login");
 		System.out.println("2. Sign Up");
-
-		int opt = Helper.readInt("Enter an option");
-		if(opt == 1) {
-			login();
-			opt = menu();
-		} else if (opt ==2) {
-			signUp();
-		} else {
-			System.out.println("Invalid option entered !!!");
-		}
-
-	}
-
-	public static void login() {
+		System.out.println("3. Exit");
+		int opt = Helper.readInt("Enter option: ");
 		
 		
-
+		
+		
 	}
 
 	public static void signUp() {
 
 	}
 
-	public static int menu() {
-		int option = Helper.readInt(null);
-		return option;
-	}
+
 
 	public static void addCurr() {
 		String country = Helper.readString("Enter country name: ");
@@ -118,6 +100,12 @@ public class MoneyExchange {
 	}
 	
 	public static void searchCurr() {
+		
+	}
+	
+	public Account verifyAccount(String usr, String pwd) {
+		
+		return accList.get(0);
 		
 	}
 
